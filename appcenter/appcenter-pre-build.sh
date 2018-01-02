8c2ba5d576e78d2744f6adea492295ab22a91547
David Siegel <dasiege@microsoft.com>
2018-01-01 16:16:16 -0800

    Sync extension version with local quicktype version

--- appcenter/appcenter-pre-build.sh
+++ appcenter/appcenter-pre-build.sh
@@ -8,4 +8,10 @@ source appcenter/slack.sh
 ### Build ###
 #############
 
+npm update quicktype
+
+# Sync extension version with quicktype
+PUBLISHED = `npm -j ls quicktype | jq -r .dependencies.quicktype.version`
+npm version $PUBLISHED --force --no-git-tag-version
+
 npm run compile
